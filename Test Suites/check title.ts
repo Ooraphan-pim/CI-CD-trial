b145abbd6006778915cde80f4262411c9bb26023
pim <pim9556@gmail.com>
2021-09-27 13:30:14 +0700

    CI-CD trial test case: check title in google
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: pim <pim9556@gmail.com>

---
+++ Test Suites/check title.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>check title</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>239120d7-5c46-4f9c-baec-a33390f98efa</testSuiteGuid>
+   <testCaseLink>
+      <guid>e255ce6f-be34-4bfd-8303-8e7de7431345</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search_check_Title</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
